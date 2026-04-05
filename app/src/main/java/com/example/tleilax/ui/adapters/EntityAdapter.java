be63cbf940ec59c525386794142881627d02d32b
Kort <olejnikmax@gmail.com>
2026-04-05 16:57:23 +0300

    minimal fix to make the app start

--- app/src/main/java/com/example/tleilax/ui/adapters/EntityAdapter.java
+++ app/src/main/java/com/example/tleilax/ui/adapters/EntityAdapter.java
@@ -1,6 +1,23 @@
 package com.example.tleilax.ui.adapters;
 
+import android.view.View;
+import android.view.ViewGroup;
+
 import androidx.recyclerview.widget.RecyclerView;
 
 public class EntityAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return new RecyclerView.ViewHolder(new View(parent.getContext())) {};
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
 }
