21b8f2b7818be90ac155c9073ddcd8fede9271e8
Kort <olejnikmax@gmail.com>
2026-04-07 22:35:03 +0300

    Add Room-backed simulation snapshot storage

--- app/src/main/java/com/example/tleilax/storage/SaveLoad.java
+++ app/src/main/java/com/example/tleilax/storage/SaveLoad.java
@@ -1,4 +1,23 @@
 package com.example.tleilax.storage;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.example.tleilax.simulation.WorldSnapshot;
+
+import java.util.List;
+
 public interface SaveLoad {
+
+    long save(@NonNull String name, @NonNull WorldSnapshot state);
+
+    @Nullable
+    WorldSnapshot load(long id);
+
+    @NonNull
+    List<SimulationSaveEntity> listSaves();
+
+    void delete(@NonNull SimulationSaveEntity saveEntity);
+
+    void deleteAll();
 }
